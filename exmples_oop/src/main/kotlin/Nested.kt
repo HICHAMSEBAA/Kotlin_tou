786d592c85f001c64326a0676161bfc153db27df
HICHAMSEBAA <hichamsebaa45@gmail.com>
2023-03-05 23:27:07 +0100

    transfert class (animel, animel2) to inner class

--- exmples_oop/src/main/kotlin/Nested.kt
+++ exmples_oop/src/main/kotlin/Nested.kt
@@ -1,35 +1,42 @@
 import kotlin.properties.Delegates
 
-class Zoo{
-    lateinit var name: String
+class Zoo(private val name: String,private val sound: String){
+    var zoo_name: String = ""
     var id: Int = 0
     fun display(){
         println("Information about this ZOO")
-        println("Name is   : ${this.name}")
+        println("Name is   : ${this.zoo_name}")
         println("ID ZOO is : ${this.id}")
     }
-    class Animel(private val name: String,private val sound: String){
+    inner class Animel{
         fun animel(){
-            println("This Animel is ${this.name} ")
-            println("This is Sound ${this.sound}")
+            println("===== information about this animel =====")
+            println("This Animel is $name ")
+            println("This is Sound $sound")
+            println("--> ZOO info : name : $zoo_name, ID : $id")
         }
     }
-    class Animel2(private val name: String, private val sound: String){
+    inner class Animel2{
         fun animel2(){
             println("====== information about this animel ======")
-            println("This Animel is ${this.name} ")
-            println("This is Sound ${this.sound}")
+            println("This Animel is $name ")
+            println("This is Sound $sound")
+            println("--> ZOO info : name : $zoo_name, ID : $id")
         }
     }
 }
 fun main(){
-    var z = Zoo()
-    z.name = "zooo parcke"
+    var z = Zoo("Rooster","cococo")
+    z.zoo_name = "zooo parcke"
     z.id = 100023332
     z.display()
-    var A = Zoo.Animel("Rooster","cococo")
+    var A = Zoo("Rooster","cococo").Animel()
+    A.zoo_name = "zooo parcke"
+    A.id = 100023332
     A.animel()
-    var A2 = Zoo.Animel2("Rooster","cococo")
+    var A2 = Zoo("Rooster","cococo").Animel2()
+    A2.zoo_name = "zooo parcke"
+    A2.id = 100023332
     A2.animel2()
 
 
