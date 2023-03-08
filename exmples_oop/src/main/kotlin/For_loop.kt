73ce288cd95c5b8d79a0f5dcb2db26fd21a97c3c
HICHAMSEBAA <hichamsebaa45@gmail.com>
2023-03-08 22:45:41 +0100

    add For_loop.kt file to the project

---
+++ exmples_oop/src/main/kotlin/For_loop.kt
@@ -0,0 +1,19 @@
+fun main(){
+    print("for (I in 1..10) print(i) = ")
+    for (i in 1..10) print("$i ")
+
+    println()
+
+    print("for (i in 10..1)print(i) = ")
+    for (i in 10..1) print("$i ")
+
+    println()
+
+    print("for (i in 10 downto 1) print(i) = ")
+    for (i in 10 downTo 1) print("$i ")
+
+    println()
+
+    print("for (i in 1..10 step 2) print(i)")
+    for (i in 1..10 step 2) print("$i ")
+}
